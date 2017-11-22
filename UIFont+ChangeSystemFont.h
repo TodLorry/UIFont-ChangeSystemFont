29bd64040b294dc54c44efd0624b4f8af5f80e2f
albus <strawberry111111@163.com>
2017-11-22 19:03:38 +0800

    add source file.

---
+++ UIFont+ChangeSystemFont.h
@@ -0,0 +1,15 @@
+//
+//  UIFont+ChangeSystemFont.h
+//  AirBrush
+//
+//  Created by albus on 2017/10/9.
+//  Copyright © 2017年 Hippo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (ChangeSystemFont)
+
+//+ (UIFont *)changedSystemFontOfSize:(CGFloat)fontSize;
+
+@end
---
+++ UIFont+ChangeSystemFont.m
@@ -0,0 +1,27 @@
+//
+//  UIFont+ChangeSystemFont.m
+//  AirBrush
+//
+//  Created by albus on 2017/10/9.
+//  Copyright © 2017年 Hippo. All rights reserved.
+//
+
+#import "UIFont+ChangeSystemFont.h"
+
+@implementation UIFont (ChangeSystemFont)
+
+//+(void)load
+//{
+//    Method newMethod = class_getClassMethod([self class], @selector(changedSystemFontOfSize:));
+//    Method oldMethod = class_getClassMethod([self class], @selector(systemFontOfSize:));
+//    
+//    //交换方法
+//    method_exchangeImplementations(newMethod, oldMethod);
+//}
+//
+//+ (UIFont *)changedSystemFontOfSize:(CGFloat)fontSize;
+//{
+//    return [UIFont fontWithName:@"Arial Rounded MT Bold" size:fontSize];
+//}
+
+@end
